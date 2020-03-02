8bde0d80bc41caa9e0c900c77a91a7414da4cb75
Marei Kikukawa <contact@calmery.me>
2020-03-02 19:14:45 +0900

    createGlobalStyle の body、font-family を削除する

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -2,6 +2,7 @@ import { createGlobalStyle } from "styled-components";
 
 export const GlobalStyle = createGlobalStyle`
   body {
-    font-family: sans-serif;
+    margin: 0;
+    padding: 0;
   }
 `;
