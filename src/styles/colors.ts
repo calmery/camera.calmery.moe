04a8327e360d73324f8250253bc95a03e84b14ab
Marei Kikukawa <contact@calmery.me>
2020-03-08 01:54:50 +0900

    余白と色に関する定数を追加する

---
+++ src/styles/colors.ts
@@ -0,0 +1,12 @@
+export const Colors = {
+  black: "#3C3C3C",
+  blue: "#91C3FF",
+  gray: "#B4B4B4",
+  lightGray: "#F0F0F0",
+  pink: "#FF91BE",
+  white: "#FFFFFF"
+};
+
+export const GradientColors = {
+  pinkToBlue: `linear-gradient(135deg, ${Colors.pink}, ${Colors.blue})`
+};
