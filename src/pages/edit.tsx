3915f6a260a4bea3e4741f39cdf9e0b1c8e039d4
Marei Kikukawa <contact@calmery.me>
2020-03-10 01:39:26 +0900

    読み込んだ画像を表示できるようにする

--- src/pages/edit.tsx
+++ src/pages/edit.tsx
@@ -1,8 +1,9 @@
 import React from "react";
 import { NextPage } from "next";
 import { withRedux, NextPageContextWithRedux } from "~/modules";
+import Canvas from "~/containers/canvas";
 
-const Edit: NextPage = () => <div>Edit: Hello World !</div>;
+const Edit: NextPage = () => <Canvas />;
 
 Edit.getInitialProps = async (_: NextPageContextWithRedux) => {
   // ToDo
