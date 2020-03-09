45991243b8a3aead7090a579f047976126490137
Marei Kikukawa <contact@calmery.me>
2020-03-10 02:35:40 +0900

    画像をダウンロードできるようにする

--- src/pages/edit.tsx
+++ src/pages/edit.tsx
@@ -1,9 +1,15 @@
 import React from "react";
 import { NextPage } from "next";
+import Link from "next/link";
 import { withRedux, NextPageContextWithRedux } from "~/modules";
 import { Canvas } from "~/containers/Canvas";
 
-const Edit: NextPage = () => <Canvas />;
+const Edit: NextPage = () => (
+  <>
+    <Canvas />
+    <Link href="/preview">Preview</Link>
+  </>
+);
 
 Edit.getInitialProps = async (_: NextPageContextWithRedux) => {
   // ToDo
