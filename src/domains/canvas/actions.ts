15269c244ba22339ba6324d2eb93af342fb24883
calmery <calmery@pixiv.com>
2020-04-15 01:38:56 +0900

    複数の画像を読み込んで表示できるようにする

--- src/domains/canvas/actions.ts
+++ src/domains/canvas/actions.ts
@@ -1,9 +1,63 @@
-export const NO_OP = "NO_OP" as const;
+import { Dispatch } from "redux";
+import blueimpLoadImage from "blueimp-load-image";
+
+export const ADD_USER_IMAGE = "ADD_USER_IMAGE" as const;
+export const REMOVE_USER_IMAGE = "REMOVE_USER_IMAGE" as const;
 
 // Actions
 
-export const noOp = () => ({
-  type: NO_OP,
+export const addUserImage = (
+  dataUrl: string,
+  width: number,
+  height: number
+) => ({
+  type: ADD_USER_IMAGE,
+  payload: {
+    dataUrl,
+    width,
+    height,
+  },
 });
 
-export type Actions = ReturnType<typeof noOp>;
+export const removeUserImage = () => ({
+  type: REMOVE_USER_IMAGE,
+});
+
+// Redux Thunk
+
+const convertUrlToImage = (url: string): Promise<HTMLImageElement> => {
+  return new Promise((resolve, reject) => {
+    const image = new Image();
+
+    image.onerror = () => reject();
+    image.onload = () => resolve(image);
+
+    image.src = url;
+  });
+};
+
+// TODO: `blueimpLoadImage` のエラー処理をちゃんとする
+export const addUserImageFromFile = (file: File) => {
+  return (dispatch: Dispatch) => {
+    return new Promise((resolve) => {
+      blueimpLoadImage(
+        file,
+        async (canvas) => {
+          const dataUrl = (canvas as HTMLCanvasElement).toDataURL();
+          const { width, height } = await convertUrlToImage(dataUrl);
+
+          dispatch(addUserImage(dataUrl, width, height));
+
+          resolve();
+        },
+        { canvas: true, orientation: true }
+      );
+    });
+  };
+};
+
+// Actions
+
+export type Actions =
+  | ReturnType<typeof addUserImage>
+  | ReturnType<typeof removeUserImage>;
--- src/domains/canvas/reducer.ts
+++ src/domains/canvas/reducer.ts
@@ -1,13 +1,74 @@
-import { Actions, NO_OP } from "./actions";
+import * as uuid from "uuid";
+import { Actions, ADD_USER_IMAGE } from "./actions";
 
-export type CanvasState = {};
+export type CanvasUserLayer = {
+  dataUrl: string;
+  width: number;
+  height: number;
+};
 
-const initialState: CanvasState = {};
+export type CanvasUserClipPath = {
+  id: string;
+  width: number;
+  height: number;
+  x: number;
+  y: number;
+  d: string;
+};
+
+export type CanvasState = {
+  width: number;
+  height: number;
+  layers: {
+    users: (CanvasUserLayer | null)[];
+  };
+  clipPaths: {
+    users: CanvasUserClipPath[];
+  };
+};
+
+const initialState: CanvasState = {
+  width: 900,
+  height: 1200,
+  layers: {
+    users: [],
+  },
+  clipPaths: {
+    users: [
+      {
+        id: uuid.v4(),
+        width: 852,
+        height: 612,
+        x: 24,
+        y: 24,
+        d: "M0 0H852V519.623L0 612V0Z",
+      },
+      {
+        id: uuid.v4(),
+        width: 852,
+        height: 612,
+        x: 24,
+        y: 564,
+        d: "M0 96L852 0V612H0V96Z",
+      },
+    ],
+  },
+};
 
 export default (state = initialState, action: Actions): CanvasState => {
+  console.log(action);
   switch (action.type) {
-    case NO_OP:
-      return {};
+    case ADD_USER_IMAGE: {
+      const { layers } = state;
+
+      return {
+        ...state,
+        layers: {
+          ...layers,
+          users: [...layers.users, action.payload],
+        },
+      };
+    }
 
     default:
       return state;
