cdd1176bc8c5b5f748ace72913d78a5dcb723e06
Marei Kikukawa <contact@calmery.me>
2020-04-27 03:22:03 +0900

    Cropper のアクションにある共通処理を切り出す

--- src/domains/cropper/actions.ts
+++ src/domains/cropper/actions.ts
@@ -1,13 +1,15 @@
+export const START_DRAG = "CROPPER/START_DRAG" as const;
+export const START_TRANSFORM = "CROPPER/START_TRANSFORM" as const;
+export const START_ROTATE_IMAGE = "CROPPER/START_ROTATE_IMAGE" as const;
 export const CHANGE_FREE_ASPECT = "CROPPER/CHANGE_FREE_ASPECT" as const;
 export const SET_ASPECT_RATIO = "CROPPER/SET_ASPECT_RATIO" as const;
 export const SET_CONTAINER_DISPLAY_SIZE = "CROPPER/SET_CONTAINER_DISPLAY_SIZE" as const;
-export const START_DRAG = "CROPPER/START_DRAG" as const;
-export const RESET_FLAGS = "CROPPER/RESET_FLAGS" as const;
-export const START_TRANSFORM = "CROPPER/START_TRANSFORM" as const;
-export const START_ROTATE_IMAGE = "CROPPER/START_ROTATE_IMAGE" as const;
 export const UPDATE = "CROPPER/UPDATE" as const;
+export const RESET_FLAGS = "CROPPER/RESET_FLAGS" as const;
 
-export const update = (
+// Helper Functions
+
+const convertEventToPositions = (
   event: MouseEvent | TouchEvent | React.MouseEvent | React.TouchEvent
 ) => {
   const positions = [];
@@ -28,11 +30,17 @@ export const update = (
     });
   }
 
-  return {
-    type: UPDATE,
-    payload: positions,
-  };
+  return positions;
 };
+
+// Actions
+
+export const update = (
+  event: MouseEvent | TouchEvent | React.MouseEvent | React.TouchEvent
+) => ({
+  type: UPDATE,
+  payload: convertEventToPositions(event),
+});
 
 export const changeFreeAspect = () => ({
   type: CHANGE_FREE_ASPECT,
@@ -58,59 +66,21 @@ export const startRotateImage = (event: TouchEvent) => ({
   payload: { event },
 });
 
-export const startTransform = (event: React.TouchEvent | React.MouseEvent) => {
-  const positions = [];
+export const startTransform = (event: React.TouchEvent | React.MouseEvent) => ({
+  type: START_TRANSFORM,
+  payload: convertEventToPositions(event),
+});
 
-  if ((event as any).touches) {
-    const { touches } = event as TouchEvent | React.TouchEvent;
-
-    for (let i = 0; i < touches.length; i++) {
-      positions.push({
-        clientX: touches[i].clientX,
-        clientY: touches[i].clientY,
-      });
-    }
-  } else {
-    positions.push({
-      clientX: (event as MouseEvent | React.MouseEvent).clientX,
-      clientY: (event as MouseEvent | React.MouseEvent).clientY,
-    });
-  }
-
-  return {
-    type: START_TRANSFORM,
-    payload: positions,
-  };
-};
-
-export const startDrag = (event: React.MouseEvent | React.TouchEvent) => {
-  const positions = [];
-
-  if ((event as any).touches) {
-    const { touches } = event as TouchEvent | React.TouchEvent;
-
-    for (let i = 0; i < touches.length; i++) {
-      positions.push({
-        clientX: touches[i].clientX,
-        clientY: touches[i].clientY,
-      });
-    }
-  } else {
-    positions.push({
-      clientX: (event as MouseEvent | React.MouseEvent).clientX,
-      clientY: (event as MouseEvent | React.MouseEvent).clientY,
-    });
-  }
-
-  return {
-    type: START_DRAG,
-    payload: positions,
-  };
-};
+export const startDrag = (event: React.MouseEvent | React.TouchEvent) => ({
+  type: START_DRAG,
+  payload: convertEventToPositions(event),
+});
 
 export const resetFlags = () => ({
   type: RESET_FLAGS,
 });
+
+// Types
 
 export type Actions =
   | ReturnType<typeof changeFreeAspect>
