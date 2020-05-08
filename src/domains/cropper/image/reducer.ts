cfe39912ef01539e44b8c6f4e5db72ac22e7eac2
Marei Kikukawa <contact@calmery.me>
2020-05-08 22:33:20 +0900

    Cropper、image の Reducer 内に処理を移動する

--- src/domains/cropper/image/reducer.ts
+++ src/domains/cropper/image/reducer.ts
@@ -1,7 +1,6 @@
-import * as actions from "./actions";
-import { TICK, Actions, COMPLETE } from "../actions";
-
-const { SET_IMAGE, START_IMAGE_TRANSFORMING } = actions;
+import { SET_IMAGE, START_IMAGE_TRANSFORMING } from "./actions";
+import { Actions, COMPLETE, TICK } from "~/domains/cropper/actions";
+import { CursorPosition } from "~/utils/convert-event-to-positions";
 
 export type CropperImageState = {
   url: string;
@@ -25,160 +24,144 @@ export type CropperImageState = {
 };
 
 const initialState: CropperImageState = {
-  isImageTransforming: false,
   url: "",
   width: 0,
   height: 0,
+  isImageTransforming: false,
   position: {
     x: 0,
     y: 0,
-  },
-  scale: {
-    current: 1,
-    previous: 1,
-    valueAtTransformStart: 0,
   },
   rotate: {
     current: 0,
     previous: 0,
     valueAtTransformStart: 0,
   },
-};
-
-// Events
-
-const setImage = ({
-  url,
-  width,
-  height,
-}: ReturnType<typeof actions.setImage>["payload"]) => ({
-  url,
-  width,
-  height,
-  isImageTransforming: false,
-  position: {
-    x: 0,
-    y: 0,
-  },
   scale: {
     current: 1,
     previous: 1,
     valueAtTransformStart: 0,
   },
-  rotate: {
-    current: 0,
-    previous: 0,
-    valueAtTransformStart: 0,
-  },
-});
-
-const startImageTransforming = (
-  state: CropperImageState,
-  positions: ReturnType<typeof actions.startImageTransforming>["payload"]
-): CropperImageState => {
-  // Multi Touch
-  if (positions.length > 1) {
-    const distanceBetweenFingers = Math.pow(
-      Math.pow(positions[1].clientX - positions[0].clientX, 2) +
-        Math.pow(positions[1].clientY - positions[0].clientY, 2),
-      0.5
-    );
-
-    const angleBetweenFingers =
-      Math.atan2(
-        positions[1].clientY - positions[0].clientY,
-        positions[1].clientX - positions[0].clientX
-      ) *
-      (180 / Math.PI);
-
-    return {
-      ...state,
-      isImageTransforming: true,
-      scale: {
-        ...state.scale,
-        previous: state.scale.current,
-        valueAtTransformStart: distanceBetweenFingers,
-      },
-      rotate: {
-        ...state.rotate,
-        previous: state.rotate.current,
-        valueAtTransformStart: angleBetweenFingers,
-      },
-    };
-  }
-
-  return state;
 };
-
-const tick = (
-  state: CropperImageState,
-  positions: { clientX: number; clientY: number }[]
-) => {
-  const { rotate, isImageTransforming } = state;
-
-  if (!isImageTransforming && positions.length < 2) {
-    return state;
-  }
-
-  const nextAngle =
-    rotate.previous +
-    (Math.atan2(
-      positions[1].clientY - positions[0].clientY,
-      positions[1].clientX - positions[0].clientX
-    ) *
-      (180 / Math.PI) -
-      rotate.valueAtTransformStart);
-  const currentLength = Math.pow(
-    Math.pow(positions[1].clientX - positions[0].clientX, 2) +
-      Math.pow(positions[1].clientY - positions[0].clientY, 2),
-    0.5
-  );
-  const nextScale =
-    (currentLength / state.scale.valueAtTransformStart) * state.scale.previous;
-  const nextX =
-    state.position.x +
-    (state.width * state.scale.current - state.width * nextScale) / 2;
-  const nextY =
-    state.position.y +
-    (state.height * state.scale.current - state.height * nextScale) / 2;
-
-  return {
-    ...state,
-    position: {
-      x: nextX,
-      y: nextY,
-    },
-    scale: {
-      ...state.scale,
-      current: nextScale,
-    },
-    rotate: {
-      ...state.rotate,
-      current: nextAngle,
-    },
-  };
-};
-
-const complete = (state: CropperImageState): CropperImageState => ({
-  ...state,
-  isImageTransforming: false,
-});
 
 // Main
 
 export default (state = initialState, action: Actions) => {
   switch (action.type) {
-    case SET_IMAGE:
-      return setImage(action.payload);
+    case SET_IMAGE: {
+      const { url, width, height } = action.payload;
 
-    case START_IMAGE_TRANSFORMING:
-      return startImageTransforming(state, action.payload);
+      return {
+        url,
+        width,
+        height,
+        isImageTransforming: false,
+        position: {
+          x: 0,
+          y: 0,
+        },
+        scale: {
+          current: 1,
+          previous: 1,
+          valueAtTransformStart: 0,
+        },
+        rotate: {
+          current: 0,
+          previous: 0,
+          valueAtTransformStart: 0,
+        },
+      };
+    }
 
-    case TICK:
-      return tick(state, action.payload.positions);
+    case START_IMAGE_TRANSFORMING: {
+      const positions = action.payload;
+
+      if (positions.length < 2) {
+        return state;
+      }
+
+      const distanceBetweenFingers = Math.pow(
+        Math.pow(positions[1].clientX - positions[0].clientX, 2) +
+          Math.pow(positions[1].clientY - positions[0].clientY, 2),
+        0.5
+      );
+
+      const angleBetweenFingers =
+        Math.atan2(
+          positions[1].clientY - positions[0].clientY,
+          positions[1].clientX - positions[0].clientX
+        ) *
+        (180 / Math.PI);
+
+      return {
+        ...state,
+        isImageTransforming: true,
+        scale: {
+          ...state.scale,
+          previous: state.scale.current,
+          valueAtTransformStart: distanceBetweenFingers,
+        },
+        rotate: {
+          ...state.rotate,
+          previous: state.rotate.current,
+          valueAtTransformStart: angleBetweenFingers,
+        },
+      };
+    }
+
+    case TICK: {
+      const { positions } = action.payload;
+      const { rotate, isImageTransforming } = state;
+
+      if (!isImageTransforming && positions.length < 2) {
+        return state;
+      }
+
+      const nextAngle =
+        rotate.previous +
+        (Math.atan2(
+          positions[1].clientY - positions[0].clientY,
+          positions[1].clientX - positions[0].clientX
+        ) *
+          (180 / Math.PI) -
+          rotate.valueAtTransformStart);
+      const currentLength = Math.pow(
+        Math.pow(positions[1].clientX - positions[0].clientX, 2) +
+          Math.pow(positions[1].clientY - positions[0].clientY, 2),
+        0.5
+      );
+      const nextScale =
+        (currentLength / state.scale.valueAtTransformStart) *
+        state.scale.previous;
+      const nextX =
+        state.position.x +
+        (state.width * state.scale.current - state.width * nextScale) / 2;
+      const nextY =
+        state.position.y +
+        (state.height * state.scale.current - state.height * nextScale) / 2;
+
+      return {
+        ...state,
+        position: {
+          x: nextX,
+          y: nextY,
+        },
+        scale: {
+          ...state.scale,
+          current: nextScale,
+        },
+        rotate: {
+          ...state.rotate,
+          current: nextAngle,
+        },
+      };
+    }
 
     case COMPLETE:
-      return complete(state);
+      return {
+        ...state,
+        isImageTransforming: false,
+      };
 
     default:
       return state;
