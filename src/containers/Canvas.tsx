243960c47e40c6653894726b19b8e032b4356b87
Marei Kikukawa <contact@calmery.me>
2020-05-11 22:29:35 +0900

    CanvasStickerLayers を FC にする

--- src/containers/Canvas.tsx
+++ src/containers/Canvas.tsx
@@ -1,7 +1,7 @@
 import React, { useRef, useEffect, useCallback } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { CanvasFilters } from "./CanvasFilters";
-import CanvasStickerLayers from "./CanvasStickerLayers";
+import { CanvasStickerLayers } from "./CanvasStickerLayers";
 import { CanvasUserLayers } from "./CanvasUserLayers";
 import { State } from "~/domains";
 import { actions } from "~/domains/canvas/actions";
--- src/containers/CanvasStickerLayers.tsx
+++ src/containers/CanvasStickerLayers.tsx
@@ -1,85 +1,76 @@
-import React from "react";
-import { connect } from "react-redux";
-import { ThunkDispatch } from "redux-thunk";
+import React, { useCallback } from "react";
+import { useSelector, useDispatch } from "react-redux";
 import CanvasStickerLayer from "~/components/CanvasStickerLayer";
 import { State } from "~/domains";
-import { actions, Actions } from "~/domains/canvas/actions";
+import { actions } from "~/domains/canvas/actions";
 
-const mapStateToProps = ({ canvas }: State) => canvas;
+export const CanvasStickerLayers: React.FC = () => {
+  const dispatch = useDispatch();
+  const { layers, displayRatio, x, y } = useSelector(
+    ({ canvas }: State) => canvas
+  );
 
-const mapDispatchToProps = (dispatch: ThunkDispatch<{}, {}, Actions>) => ({
-  startCropperTransforming: (index: number, previousLength: number) =>
-    dispatch(actions.startCanvasStickerLayerTransform(index, previousLength)),
-  startMultiTouchingTransform: (
-    index: number,
-    previousLength: number,
-    startingAngle: number
-  ) => {
-    dispatch(
-      actions.startCanvasStickerLayerMultiTouchingTransform(
-        index,
-        previousLength,
-        startingAngle
-      )
-    );
-  },
-  startCropperMoving: (
-    index: number,
-    referenceX: number,
-    referenceY: number
-  ) => {
-    dispatch(
-      actions.startCanvasStickerLayerDrag(index, referenceX, referenceY)
-    );
-  },
-  onClick: (index: number) => {
-    dispatch(actions.changeActiveCanvasStickerLayer(index));
-  },
-  removeCanvasStickerLayer: () => {
+  const handleOnstartCropperTransforming = useCallback(
+    (index: number, previousLength: number) => {
+      dispatch(actions.startCanvasStickerLayerTransform(index, previousLength));
+    },
+    [dispatch]
+  );
+
+  const handleOnStartMultiTouchingTransform = useCallback(
+    (index: number, previousLength: number, startingAngle: number) => {
+      dispatch(
+        actions.startCanvasStickerLayerMultiTouchingTransform(
+          index,
+          previousLength,
+          startingAngle
+        )
+      );
+    },
+    [dispatch]
+  );
+
+  const handleOnstartCropperMoving = useCallback(
+    (index: number, referenceX: number, referenceY: number) => {
+      dispatch(
+        actions.startCanvasStickerLayerDrag(index, referenceX, referenceY)
+      );
+    },
+    [dispatch]
+  );
+
+  const handleOnClick = useCallback(
+    (index: number) => {
+      dispatch(actions.changeActiveCanvasStickerLayer(index));
+    },
+    [dispatch]
+  );
+
+  const removeCanvasStickerLayer = useCallback(() => {
     dispatch(actions.removeCanvasStickerLayer());
-  },
-});
+  }, [dispatch]);
 
-class CanvasStickerLayers extends React.Component<
-  ReturnType<typeof mapStateToProps> & ReturnType<typeof mapDispatchToProps>
-> {
-  public render = () => {
-    const { layers, displayRatio, x, y, removeCanvasStickerLayer } = this.props;
-
-    return layers.stickers.map((sticker, index) => (
-      <CanvasStickerLayer
-        key={index}
-        selected={index === layers.stickers.length - 1}
-        {...sticker}
-        displayRatio={displayRatio}
-        startCropperTransforming={(l) =>
-          this.handleOnstartCropperTransforming(index, l)
-        }
-        startMultiTouchingTransform={(l, a) =>
-          this.handleOnStartMultiTouchingTransform(index, l, a)
-        }
-        startCropperMoving={(x, y) =>
-          this.handleOnstartCropperMoving(index, x, y)
-        }
-        canvasBaseX={x}
-        canvasBaseY={y}
-        onClick={() => this.handleOnClick(index)}
-        onClickRemoveButton={removeCanvasStickerLayer}
-      />
-    ));
-  };
-
-  // Events
-
-  private handleOnstartCropperTransforming = this.props
-    .startCropperTransforming;
-  private handleOnStartMultiTouchingTransform = this.props
-    .startMultiTouchingTransform;
-  private handleOnstartCropperMoving = this.props.startCropperMoving;
-  private handleOnClick = this.props.onClick;
-}
-
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(CanvasStickerLayers);
+  return (
+    <>
+      {layers.stickers.map((sticker, index) => (
+        <CanvasStickerLayer
+          key={index}
+          selected={index === layers.stickers.length - 1}
+          {...sticker}
+          displayRatio={displayRatio}
+          startCropperTransforming={(l) =>
+            handleOnstartCropperTransforming(index, l)
+          }
+          startMultiTouchingTransform={(l, a) =>
+            handleOnStartMultiTouchingTransform(index, l, a)
+          }
+          startCropperMoving={(x, y) => handleOnstartCropperMoving(index, x, y)}
+          canvasBaseX={x}
+          canvasBaseY={y}
+          onClick={() => handleOnClick(index)}
+          onClickRemoveButton={removeCanvasStickerLayer}
+        />
+      ))}
+    </>
+  );
+};
