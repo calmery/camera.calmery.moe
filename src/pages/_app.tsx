9167e9b8d5962aa01bf069ac8675a9274ee09ff4
Marei Kikukawa <contact@calmery.me>
2020-05-14 23:44:41 +0900

    スマートフォント UI を追加する

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -12,6 +12,20 @@ export const GlobalStyle = createGlobalStyle`
     margin: 0;
     padding: 0;
   }
+
+  /*
+   * "スマートフォントUI" licensed under the IPA Font License Agreement v1.0
+   * https://www.flopdesign.com/freefont/smartfont.html
+   * https://ipafont.ipa.go.jp/old/ipaexfont/download.html IPAexゴシック（Ver.002.01）
+   * https://ipafont.ipa.go.jp/ipa_font_license_v1.html
+   */
+  @font-face {
+    font-family: SmartFontUI;
+    font-display: swap;
+    src: url(fonts/SmartFontUI.woff2) format("woff2"),
+      url(fonts/SmartFontUI.woff) format("woff"),
+      url(fonts/SmartFontUI.ttf) format("truetype");
+  }
 `;
 
 class App extends NextApp {
