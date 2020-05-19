4d7339836c343ed3c24429b9cf0bfcd64e1973bf
Marei Kikukawa <contact@calmery.me>
2020-05-20 01:13:11 +0900

    collage ページを追加する

---
+++ src/containers/CanvasFrames.tsx
@@ -0,0 +1,114 @@
+import React from "react";
+import { useDispatch } from "react-redux";
+import styled, { css } from "styled-components";
+import { Typography } from "~/styles/typography";
+import { Spacing } from "~/styles/spacing";
+import { actions } from "~/domains/canvas/actions";
+import {
+  canvasUserLayerFrame,
+  CanvasUserLayerFrame,
+} from "~/domains/canvas/frames";
+import { GradientColors } from "~/styles/colors";
+
+const Horizontal = styled.div`
+  width: 100%;
+  overflow-x: scroll;
+  scrollbar-width: none;
+  -ms-overflow-style: none;
+
+  &::-webkit-scrollbar {
+    display: none;
+  }
+`;
+
+const HorizontalInner = styled.div`
+  width: fit-content;
+  display: flex;
+  padding: 0 ${Spacing.l}px;
+`;
+
+const Container = styled.div`
+  margin-bottom: ${Spacing.l}px;
+`;
+
+const CollageButton = styled.div<{ selected?: boolean }>`
+  width: 36px;
+  height: 36px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  cursor: pointer;
+  border-radius: 100%;
+  margin-left: ${Spacing.s}px;
+
+  &:first-child {
+    margin-left: 0;
+  }
+
+  object {
+    width: 22px;
+    height: 22px;
+    pointer-events: none;
+  }
+
+  ${({ selected }) =>
+    selected &&
+    css`
+      background: ${GradientColors.pinkToBlue};
+
+      object {
+        filter: brightness(0) invert(1);
+      }
+    `}
+`;
+
+export const CanvasFrames: React.FC = () => {
+  const dispatch = useDispatch();
+  const handleOnClickEnableCollage = (
+    frame: CanvasUserLayerFrame,
+    index: number
+  ) => dispatch(actions.enableCollage(frame, index));
+  const handleOnClickDisableCollage = () => dispatch(actions.disableCollage());
+
+  return (
+    <Container>
+      <Horizontal>
+        <HorizontalInner>
+          <CollageButton selected onClick={handleOnClickDisableCollage}>
+            <object type="image/svg+xml" data={`/images/collage/disable.svg`} />
+          </CollageButton>
+          {canvasUserLayerFrame[CanvasUserLayerFrame.W3H4].frames.map(
+            (_, index) => (
+              <CollageButton
+                onClick={() =>
+                  handleOnClickEnableCollage(CanvasUserLayerFrame.W3H4, index)
+                }
+                key={index}
+              >
+                <object
+                  type="image/svg+xml"
+                  data={`/images/collage/3-4-${index}.svg`}
+                />
+              </CollageButton>
+            )
+          )}
+          {canvasUserLayerFrame[CanvasUserLayerFrame.W4H3].frames.map(
+            (_, index) => (
+              <CollageButton
+                onClick={() =>
+                  handleOnClickEnableCollage(CanvasUserLayerFrame.W4H3, index)
+                }
+                key={index}
+              >
+                <object
+                  type="image/svg+xml"
+                  data={`/images/collage/4-3-${index}.svg`}
+                />
+              </CollageButton>
+            )
+          )}
+        </HorizontalInner>
+      </Horizontal>
+    </Container>
+  );
+};
---
+++ src/pages/collage.tsx
@@ -0,0 +1,87 @@
+import React, { useEffect, useRef } from "react";
+import { NextPage } from "next";
+import { useDispatch } from "react-redux";
+import ResizeObserver from "resize-observer-polyfill";
+import styled from "styled-components";
+import { Canvas } from "~/containers/Canvas";
+import { Menu } from "~/components/Menu";
+import { Page } from "~/components/Page";
+import { withRedux } from "~/domains";
+import { actions } from "~/domains/canvas/actions";
+import { Spacing } from "~/styles/spacing";
+import { CanvasFrames } from "~/containers/CanvasFrames";
+
+const FlexColumn = styled.div`
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+`;
+
+const ControlBar = styled.div`
+  height: 16px;
+  padding: ${Spacing.l}px;
+  flex-shrink: 0;
+
+  img {
+    width: 16px;
+    height: 16px;
+  }
+`;
+
+const CanvasContainer = styled.div`
+  box-sizing: border-box;
+  padding: 0 24px;
+  flex-grow: 1;
+  height: fit-content;
+`;
+
+const CanvasSizeDetector = styled.div`
+  width: 100%;
+  height: 100%;
+`;
+
+const BottomBar = styled.div`
+  width: 100%;
+  flex-shrink: 0;
+  padding-top: ${Spacing.l}px;
+`;
+
+const Edit: NextPage = () => {
+  const ref = useRef<HTMLDivElement>(null);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const e = ref.current!;
+    const resizeObserver = new ResizeObserver(() => {
+      dispatch(actions.updateCanvasContainerRect(e.getBoundingClientRect()));
+    });
+
+    resizeObserver.observe(e);
+
+    return () => {
+      resizeObserver.unobserve(e);
+    };
+  }, []);
+
+  return (
+    <>
+      <Page>
+        <FlexColumn>
+          <ControlBar>
+            <img src="/images/close.svg" />
+          </ControlBar>
+          <CanvasContainer>
+            <CanvasSizeDetector ref={ref} />
+          </CanvasContainer>
+          <BottomBar>
+            <CanvasFrames />
+            <Menu />
+          </BottomBar>
+        </FlexColumn>
+      </Page>
+      <Canvas />
+    </>
+  );
+};
+
+export default withRedux(Edit);
