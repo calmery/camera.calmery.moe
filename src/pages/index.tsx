ed519ece52a50012d3225b4aa281f91216e17020
Marei Kikukawa <contact@calmery.me>
2020-05-24 02:22:06 +0900

    Input コンポーネントを追加する

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -16,6 +16,7 @@ import { Button } from "~/components/Button";
 import { useDispatch } from "react-redux";
 import { withRedux } from "~/domains";
 import { actions, thunkActions } from "~/domains/canvas/actions";
+import { Input } from "~/components/Input";
 
 const Columns = styled.div`
   height: 100%;
@@ -220,6 +221,8 @@ const Index: NextPage = () => {
           <Logo>
             <Image src="/images/logos/camera.calmery.moe.svg" />
           </Logo>
+
+          <Input />
 
           <Buttons>
             <Button primary>
