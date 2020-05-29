c3df128f63e5ab80b9ab0d842323af4f30687aae
Marei Kikukawa <contact@calmery.me>
2020-05-30 03:55:44 +0900

    Page コンポーネントに position: fixed を追加する

--- src/components/Page.tsx
+++ src/components/Page.tsx
@@ -7,6 +7,10 @@ export const Page = styled.div<{ margin?: boolean }>`
   background: ${GradientColors.page};
   box-sizing: border-box;
   height: inherit;
+  width: 100%;
+  height: 100%;
+  position: fixed;
+  overflow: hidden;
 
   ${Media.queries.pc} {
     padding: 0 ${({ margin }) => (margin ? `${Spacing.xl}px` : 0)};
