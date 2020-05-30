d950d8528c0dbdcb0e0cf23b5023c7562199e951
Marei Kikukawa <contact@calmery.me>
2020-05-31 01:42:48 +0900

    クロップした画像に角度などを指定して回転、拡大縮小できるようにする

--- src/components/CanvasUserLayer.tsx
+++ src/components/CanvasUserLayer.tsx
@@ -51,49 +51,70 @@ export const CanvasUserLayerComponent: React.FC<{
       </clipPath>
 
       <g clipPath={`url(#canvas-user-layer-frame-${id})`}>
-        {/* TODO: 切り取った画像がめっちゃ小さくなることがある、1000px 以下の画像（適当に変更する）は拡大表示したい */}
         <svg
-          width={layer.croppedWidth}
-          height={layer.croppedHeight}
+          overflow="visible"
           x={isCollaging ? layer.x : 0}
           y={isCollaging ? layer.y : 0}
-          viewBox={`${layer.croppedX} ${layer.croppedY} ${layer.croppedWidth} ${layer.croppedHeight}`}
           xmlns="http://www.w3.org/2000/svg"
           xmlnsXlink="http://www.w3.org/1999/xlink"
         >
-          {/* TODO: ヤバイですね☆ */}
-          <svg
-            width={layer.width * layer.croppedScale}
-            height={layer.height * layer.croppedScale}
-            x={layer.croppedImageX}
-            y={layer.croppedImageY}
-            viewBox={`0 0 ${layer.width} ${layer.height}`}
-            xmlns="http://www.w3.org/2000/svg"
-            xmlnsXlink="http://www.w3.org/1999/xlink"
-            overflow="visible"
+          <g
+            transform={
+              isCollaging
+                ? `translate(-${
+                    (layer.croppedWidth * layer.scale - layer.croppedWidth) / 2
+                  }, -${
+                    (layer.croppedHeight * layer.scale - layer.croppedHeight) /
+                    2
+                  }) scale(${layer.scale}) rotate(${layer.angle}, ${
+                    layer.croppedWidth / 2
+                  }, ${layer.croppedHeight / 2})`
+                : undefined
+            }
           >
-            <g
-              transform={`rotate(${layer.croppedAngle}, ${layer.width / 2}, ${
-                layer.height / 2
-              })`}
+            {/* TODO: 切り取った画像がめっちゃ小さくなることがある、1000px 以下の画像（適当に変更する）は拡大表示したい */}
+            <svg
+              width={layer.croppedWidth}
+              height={layer.croppedHeight}
+              viewBox={`${layer.croppedX} ${layer.croppedY} ${layer.croppedWidth} ${layer.croppedHeight}`}
+              xmlns="http://www.w3.org/2000/svg"
+              xmlnsXlink="http://www.w3.org/1999/xlink"
             >
-              <image
-                xlinkHref={layer.dataUrl}
-                filter={`url(#canvas-user-layer-filter-${id})`}
-                width="100%"
-                height="100%"
-              />
-            </g>
-          </svg>
-          {props.stickers && stickerLayer && (
-            <CanvasStickerLayerBorder
-              baseX={(isCollaging ? layer.x : 0) + frame.x - layer.croppedX}
-              baseY={(isCollaging ? layer.y : 0) + frame.y - layer.croppedY}
-              displayMagnification={displayMagnification}
-              stickerLayer={stickerLayer}
-              backgroundBrightness={layer.dominantColorLightness}
-            />
-          )}
+              {/* TODO: ヤバイですね☆ */}
+              <svg
+                width={layer.width * layer.croppedScale}
+                height={layer.height * layer.croppedScale}
+                x={layer.croppedImageX}
+                y={layer.croppedImageY}
+                viewBox={`0 0 ${layer.width} ${layer.height}`}
+                xmlns="http://www.w3.org/2000/svg"
+                xmlnsXlink="http://www.w3.org/1999/xlink"
+                overflow="visible"
+              >
+                <g
+                  transform={`rotate(${layer.croppedAngle}, ${
+                    layer.width / 2
+                  }, ${layer.height / 2})`}
+                >
+                  <image
+                    xlinkHref={layer.dataUrl}
+                    filter={`url(#canvas-user-layer-filter-${id})`}
+                    width="100%"
+                    height="100%"
+                  />
+                </g>
+              </svg>
+              {/* {props.stickers && stickerLayer && (
+                <CanvasStickerLayerBorder
+                  baseX={(isCollaging ? layer.x : 0) + frame.x - layer.croppedX}
+                  baseY={(isCollaging ? layer.y : 0) + frame.y - layer.croppedY}
+                  displayMagnification={displayMagnification}
+                  stickerLayer={stickerLayer}
+                  backgroundBrightness={layer.dominantColorLightness}
+                />
+              )} */}
+            </svg>
+          </g>
         </svg>
       </g>
 
