0b3b761be83d8edf0a6783d3122a09b390a69002
Marei Kikukawa <contact@calmery.me>
2020-06-04 09:55:44 +0900

    リファクタリング

--- src/containers/CropperImage.tsx
+++ src/containers/CropperImage.tsx
@@ -2,6 +2,39 @@ import React from "react";
 import { useSelector } from "react-redux";
 import { State } from "~/domains";
 import { Constants } from "~/styles/constants";
+import { CropperImageState } from "~/domains/cropper/reducer";
+
+const Image: React.FC<{
+  image: CropperImageState;
+  fill?: boolean;
+}> = ({ image, fill = false }) => (
+  <svg
+    width={image.width * image.scale.current}
+    height={image.height * image.scale.current}
+    x={image.position.x}
+    y={image.position.y}
+    viewBox={`0 0 ${image.width} ${image.height}`}
+    xmlns="http://www.w3.org/2000/svg"
+    xmlnsXlink="http://www.w3.org/1999/xlink"
+    overflow="visible"
+  >
+    <g
+      transform={`rotate(${image.rotate.current}, ${image.width / 2}, ${
+        image.height / 2
+      })`}
+    >
+      <image xlinkHref={image.url} width="100%" height="100%" />
+      {fill && (
+        <rect
+          width="100%"
+          height="100%"
+          fill="#000"
+          fillOpacity={Constants.opacity}
+        />
+      )}
+    </g>
+  </svg>
+);
 
 export const CropperImage: React.FC = () => {
   const { cropper, image } = useSelector(({ cropper }: State) => cropper);
@@ -16,32 +49,7 @@ export const CropperImage: React.FC = () => {
 
   return (
     <>
-      <svg
-        width={image.width * image.scale.current}
-        height={image.height * image.scale.current}
-        x={image.position.x}
-        y={image.position.y}
-        viewBox={`0 0 ${image.width} ${image.height}`}
-        xmlns="http://www.w3.org/2000/svg"
-        xmlnsXlink="http://www.w3.org/1999/xlink"
-        overflow="visible"
-      >
-        <g
-          transform={`rotate(${image.rotate.current}, ${image.width / 2}, ${
-            image.height / 2
-          })`}
-        >
-          <image xlinkHref={image.url} width="100%" height="100%" />
-          <rect
-            width="100%"
-            height="100%"
-            fill="#000"
-            fillOpacity={Constants.opacity}
-          />
-        </g>
-      </svg>
-
-      <clipPath id="cropper-clip-path">
+      <clipPath id="cropper-image-clip-path">
         <rect
           x={cropper.position.x}
           y={cropper.position.y}
@@ -50,26 +58,10 @@ export const CropperImage: React.FC = () => {
         />
       </clipPath>
 
-      <g clipPath="url(#cropper-clip-path)">
-        <svg
-          width={image.width * image.scale.current}
-          height={image.height * image.scale.current}
-          x={image.position.x}
-          y={image.position.y}
-          viewBox={`0 0 ${image.width} ${image.height}`}
-          xmlns="http://www.w3.org/2000/svg"
-          xmlnsXlink="http://www.w3.org/1999/xlink"
-          overflow="visible"
-        >
-          <g
-            transform={`rotate(${image.rotate.current}, ${image.width / 2}, ${
-              image.height / 2
-            })`}
-          >
-            <rect fill="#fff" width={image.width} height={image.height} />
-            <image xlinkHref={image.url} width="100%" height="100%" />
-          </g>
-        </svg>
+      <Image image={image} fill />
+
+      <g clipPath="url(#cropper-image-clip-path)">
+        <Image image={image} />
       </g>
     </>
   );
