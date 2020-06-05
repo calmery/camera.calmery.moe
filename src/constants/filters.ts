0510080ff74f20673d75609997306ecbb9fed45f
Marei Kikukawa <contact@calmery.me>
2020-06-06 02:19:36 +0900

    Filter を PresetFilter にリネーム、types 配下に移動する

--- src/constants/filters.ts
+++ src/constants/filters.ts
@@ -1,85 +1,74 @@
+import { PresetFilter } from "~/types/PresetFilter";
+
 // https://silvia-odwyer.github.io/photon/
 // silvia-odwyer/photon: Rust/WebAssembly image processing library (https://github.com/silvia-odwyer/photon)
 // https://github.com/silvia-odwyer/photon/blob/4db0d5912a3ab7231bf5109ba0628f843d5a4872/crate/src/filters.rs#L107-L146
 // https://github.com/silvia-odwyer/photon/blob/4db0d5912a3ab7231bf5109ba0628f843d5a4872/crate/src/colour_spaces.rs#L575-L599
 
-export enum Filter {
-  OCEANIC = "oceanic",
-  ISLANDS = "islands",
-  MARINE = "marine",
-  SEAGREEN = "seagreen",
-  FLAGBLUE = "flagblue",
-  DIAMANTE = "diamante",
-  LIQUID = "liquid",
-  VINTAGE = "vintage", // mix_with_colour(img, vintage_rgb, 0.2)
-  PERFUME = "perfume", // mix_with_colour(img, perfume_rgb, 0.2)
-  SERENITY = "serenity", // mix_with_colour(img, serenity_rgb, 0.2)
-}
-
 // Photon のフィルターの値を使っている
-export const FILTER_PRESETS: {
-  [key in Filter]: {
+export const PRESET_FILTERS: {
+  [key in PresetFilter]: {
     r: number;
     g: number;
     b: number;
     a: number;
   };
 } = {
-  [Filter.OCEANIC]: {
+  [PresetFilter.OCEANIC]: {
     r: 0,
     g: 89,
     b: 173,
     a: 0.2,
   },
-  [Filter.ISLANDS]: {
+  [PresetFilter.ISLANDS]: {
     r: 0,
     g: 24,
     b: 95,
     a: 0.2,
   },
-  [Filter.MARINE]: {
+  [PresetFilter.MARINE]: {
     r: 0,
     g: 14,
     b: 119,
     a: 0.2,
   },
-  [Filter.SEAGREEN]: {
+  [PresetFilter.SEAGREEN]: {
     r: 0,
     g: 68,
     b: 62,
     a: 0.2,
   },
-  [Filter.FLAGBLUE]: {
+  [PresetFilter.FLAGBLUE]: {
     r: 0,
     g: 0,
     b: 131,
     a: 0.2,
   },
-  [Filter.DIAMANTE]: {
+  [PresetFilter.DIAMANTE]: {
     r: 30,
     g: 82,
     b: 87,
     a: 0.1,
   },
-  [Filter.LIQUID]: {
+  [PresetFilter.LIQUID]: {
     r: 0,
     g: 10,
     b: 75,
     a: 0.2,
   },
-  [Filter.VINTAGE]: {
+  [PresetFilter.VINTAGE]: {
     r: 120,
     g: 70,
     b: 13,
     a: 0.2,
   },
-  [Filter.PERFUME]: {
+  [PresetFilter.PERFUME]: {
     r: 80,
     g: 40,
     b: 120,
     a: 0.2,
   },
-  [Filter.SERENITY]: {
+  [PresetFilter.SERENITY]: {
     r: 10,
     g: 40,
     b: 90,
---
+++ src/types/PresetFilter.ts
@@ -0,0 +1,12 @@
+export enum PresetFilter {
+  OCEANIC = "oceanic",
+  ISLANDS = "islands",
+  MARINE = "marine",
+  SEAGREEN = "seagreen",
+  FLAGBLUE = "flagblue",
+  DIAMANTE = "diamante",
+  LIQUID = "liquid",
+  VINTAGE = "vintage",
+  PERFUME = "perfume",
+  SERENITY = "serenity",
+}
