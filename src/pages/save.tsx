7c32809ffd517c45512a49d9a3be68c69cfd0a1c
Marei Kikukawa <12670155+calmery@users.noreply.github.com>
2020-09-15 22:31:24 +0900

    save ページに Twitter へのシェアボタンを追加する

--- src/pages/save.tsx
+++ src/pages/save.tsx
@@ -3,6 +3,7 @@ import { useRouter } from "next/router";
 import React, { useCallback, useState } from "react";
 import { useSelector } from "react-redux";
 import styled from "styled-components";
+import { Button } from "~/components/Button";
 import { ControlBar } from "~/components/ControlBar";
 import { FirstLanding } from "~/components/FirstLanding";
 import { Menu } from "~/components/Menu";
@@ -16,23 +17,34 @@ import {
 import { Canvas } from "~/containers/Canvas";
 import { withRedux, State } from "~/domains";
 import * as GA from "~/utils/google-analytics";
+import { Spacing } from "~/styles/spacing";
+import { Popup } from "~/components/Popup";
 
 // Styles
 
-const Image = styled.img`
+const PreviewImage = styled.img`
   position: fixed;
+`;
+
+const ButtonContainer = styled.div`
+  max-width: 360px;
+  margin: 0 auto;
+  padding: ${Spacing.m}px ${Spacing.l}px;
+  padding-top: 0;
 `;
 
 // Components
 
 const Save: NextPage = () => {
-  const { pathname } = useRouter();
+  const { pathname, push } = useRouter();
   const canvas = useSelector(({ canvas }: State) => canvas);
 
   // States
 
   const [isTutorial, setTutorial] = useState(false);
   const [previewUrl, setPreviewUrl] = useState<string | null>(null);
+  const [isImageUploading, setIsImageUploading] = useState(false);
+  const [isImageUploadError, setImageUploadError] = useState(false);
 
   // Events
 
@@ -55,11 +67,57 @@ const Save: NextPage = () => {
     GA.saveCanvas();
   }, []);
 
+  const handleOnClickShareButton = useCallback(async () => {
+    if (!previewUrl) {
+      return;
+    }
+
+    setIsImageUploading(true);
+
+    const array = previewUrl.split(",");
+    const type = array[0]!.match(/:(.*?);/)![1];
+    const bstr = atob(array[1]);
+
+    let i = bstr.length;
+    const uInt8Array = new Uint8Array(i);
+    while (i--) {
+      uInt8Array[i] = bstr.charCodeAt(i);
+    }
+
+    const file = new File([uInt8Array], "preview.png", { type });
+
+    const formData = new FormData();
+    formData.append("image", file);
+
+    try {
+      const { data } = await fetch("http://localhost:5000/camera/images", {
+        body: formData,
+        mode: "cors",
+        method: "POST",
+      }).then((r) => r.json());
+
+      const a = document.createElement("a");
+
+      const url = `${window.location.origin}/share/${data.id}`;
+      a.href = `http://twitter.com/share?url=${url}&related=calmeryme&hashtags=%E3%81%8B%E3%82%8B%E3%82%81%E3%82%8A%E3%81%A1%E3%82%83%E3%82%93%E3%82%AB%E3%83%A1%E3%83%A9`;
+      a.target = "_blank";
+      a.rel = "noopener noreferrer";
+
+      a.click();
+    } catch (_) {
+      setImageUploadError(true);
+    } finally {
+      setIsImageUploading(false);
+    }
+  }, [previewUrl]);
+
   // Render
 
   const { userLayers, styleTop, styleLeft, styleWidth, styleHeight } = canvas;
 
   const isImageExists = userLayers.some((u) => u);
+
+  console.log(isImageUploading);
 
   return (
     <>
@@ -71,7 +129,7 @@ const Save: NextPage = () => {
             onCreatePreviewUrl={isImageExists ? setPreviewUrl : undefined}
           />
           {isImageExists && (
-            <Image
+            <PreviewImage
               style={{
                 top: styleTop,
                 left: styleLeft,
@@ -86,7 +144,19 @@ const Save: NextPage = () => {
               src={previewUrl || ""}
             />
           )}
-          <Menu />
+          <Menu>
+            {isImageExists && (
+              <ButtonContainer>
+                <Button
+                  disabled={!previewUrl || isImageUploading}
+                  primary
+                  onClickButton={handleOnClickShareButton}
+                >
+                  Twitter にシェアする！
+                </Button>
+              </ButtonContainer>
+            )}
+          </Menu>
         </PageColumn>
       </Page>
 
@@ -102,6 +172,18 @@ const Save: NextPage = () => {
           onComplete={handleOnCompleteTutorial}
           onStop={handleOnStopTutorial}
         />
+      )}
+
+      {isImageUploadError && (
+        <Popup
+          characterImageUrl="/images/stickers/2/6.png"
+          onEnter={() => setImageUploadError(false)}
+          enterText="わかった"
+        >
+          画像のアップロードに失敗したみたい...
+          <br />
+          もう一度試してみてね！
+        </Popup>
       )}
     </>
   );
