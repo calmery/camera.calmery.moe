6c1a4ba85d67cfefe6444ddd527d7c4d1f4114b8
Marei Kikukawa <12670155+calmery@users.noreply.github.com>
2021-03-02 01:37:39 +0900

    src/styles を削除、Emotion を使えるようにする

--- next-env.d.ts
+++ next-env.d.ts
@@ -1,2 +1,3 @@
+/// <reference types="@emotion/react/types/css-prop"/>
 /// <reference types="next" />
 /// <reference types="next/types/global" />
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,33 @@
+import { Global, css } from "@emotion/react";
 import { AppProps } from "next/app";
 import React from "react";
-import "../styles/globals.css";
+
+const global = css`
+  html,
+  body {
+    font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
+      Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
+    margin: 0;
+    padding: 0;
+  }
+
+  a {
+    color: inherit;
+    text-decoration: none;
+  }
+
+  * {
+    box-sizing: border-box;
+  }
+`;
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Global styles={global} />
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
