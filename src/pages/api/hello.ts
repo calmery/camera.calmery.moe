9d9a21ca20f10d924e104284f23213ec744cc4ea
Marei Kikukawa <12670155+calmery@users.noreply.github.com>
2021-03-02 01:17:30 +0900

    ESLint と Prettier を追加、`npm run fix` で実行できるようにする

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -3,5 +3,5 @@ import { VercelRequest, VercelResponse } from "@vercel/node";
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default (_: VercelRequest, res: VercelResponse) => {
-  res.status(200).json({ name: 'John Doe' })
-}
+  res.status(200).json({ name: "John Doe" });
+};
