1ceb86b927512fc5f1ad904c69bd77e2bbd4837a
Marei Kikukawa <12670155+calmery@users.noreply.github.com>
2021-03-02 00:49:47 +0900

    TypeScript を使えるようにする

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -1,5 +1,7 @@
+import { VercelRequest, VercelResponse } from "@vercel/node";
+
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-export default (req, res) => {
+export default (_: VercelRequest, res: VercelResponse) => {
   res.status(200).json({ name: 'John Doe' })
 }
