1c92b9c060d4c24b2847e2a5fdf1b402bc6360b1
Marei Kikukawa <12670155+calmery@users.noreply.github.com>
2021-03-02 10:46:03 +0900

    `~` で src フォルダを参照できるようにする

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
+const webpack = (config, options) => {
+  config.resolve.alias["~"] = path.resolve(__dirname, "src");
+
+  return config;
+}
+
 module.exports = {
   poweredByHeader: false,
   productionBrowserSourceMaps: true,
   reactStrictMode: true,
   trailingSlash: false,
+  webpack,
 };
