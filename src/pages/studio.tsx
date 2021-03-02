7524623e170db4e0b3770144568ba54a554e4ef8
Marei Kikukawa <12670155+calmery@users.noreply.github.com>
2021-03-03 02:16:08 +0900

    locales/ja/common.json から型を生成、t 関数のキーに型を付ける

--- src/pages/studio.tsx
+++ src/pages/studio.tsx
@@ -1,10 +1,10 @@
 import { GetServerSideProps, NextPage } from "next";
-import useTranslation from "next-translate/useTranslation";
 import React from "react";
+import { useTranslation } from "~/hooks/useTranslation";
 
 const Studio: NextPage = () => {
-  const { t } = useTranslation("common");
-  return <div>Calmery-Chan Camera Studio {t("message")}</div>;
+  const { t } = useTranslation();
+  return <div>{t("message")}</div>;
 };
 
 export const getServerSideProps: GetServerSideProps = async () => {
