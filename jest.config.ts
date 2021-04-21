a3e9bd4309ab87b6edbd89de50ccf9f1a9eab61e
Marei Kikukawa <12670155+calmery@users.noreply.github.com>
2021-04-22 01:24:52 +0900

    Jest でも `~` でファイルを参照できるように、setupTests.js をリネームする

--- jest.config.ts
+++ jest.config.ts
@@ -1,6 +1,9 @@
 export default {
   collectCoverageFrom: ["**/*.{ts,tsx}", "!**/*.d.ts", "!**/node_modules/**"],
-  setupFilesAfterEnv: ["<rootDir>/jest/setupTests.js"],
+  moduleNameMapper: {
+    "^~/(.+)": "<rootDir>/src/$1",
+  },
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
   testPathIgnorePatterns: ["/.next/", "/node_modules/"],
   transform: {
     "^.+\\.(js|ts|tsx)$": "<rootDir>/node_modules/babel-jest",
