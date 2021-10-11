d3c7e47dc8e43d1d1b318b2bb7905a5401180b8d
calmery <12670155+calmery@users.noreply.github.com>
2021-10-11 15:54:15 +0900

    @calmery-chan/aleph-plugin-tailwindcss を使うように変更する

--- aleph.config.ts
+++ aleph.config.ts
@@ -1,6 +1,6 @@
 import type { Config } from "aleph/types.d.ts";
 import emotion from "./node_modules/@calmery-chan/aleph-plugin-emotion/mod.ts";
-import tailwindcss from "./plugins/tailwindcss.ts";
+import tailwindcss from "./node_modules/@calmery-chan/aleph-plugin-tailwindcss/mod.ts";
 
 export default <Config> {
   build: {
