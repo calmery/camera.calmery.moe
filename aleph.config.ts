9662a041387581e019c9a54b80f57fb99da84d6e
calmery <12670155+calmery@users.noreply.github.com>
2021-10-14 15:23:55 +0900

    @calmery-chan/aleph-plugin-tailwindcss を deno.land/x/ から使う

--- aleph.config.ts
+++ aleph.config.ts
@@ -1,6 +1,6 @@
 import type { Config } from "aleph/types.d.ts";
 import emotion from "./node_modules/@calmery-chan/aleph-plugin-emotion/mod.ts";
-import tailwindcss from "./node_modules/@calmery-chan/aleph-plugin-tailwindcss/mod.ts";
+import tailwindcss from "https://deno.land/x/calmery_chan_aleph_plugin_tailwindcss@v2.0.0/plugin.ts";
 
 export default <Config> {
   build: {
