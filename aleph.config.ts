55a5b45d0bb9f74c735147a83b45d2fa9472e9c9
calmery <12670155+calmery@users.noreply.github.com>
2021-10-19 05:22:18 +0900

    @calmery-chan/aleph-plugin-emotion のバージョンを 2.0.0 にする

--- aleph.config.ts
+++ aleph.config.ts
@@ -1,5 +1,5 @@
 import type { Config } from "aleph/types.d.ts";
-import emotion from "./node_modules/@calmery-chan/aleph-plugin-emotion/mod.ts";
+import emotion from "./node_modules/@calmery-chan/aleph-plugin-emotion/plugin.ts";
 import tailwindcss from "https://deno.land/x/calmery_chan_aleph_plugin_tailwindcss@v2.0.0/plugin.ts";
 
 export default <Config> {
