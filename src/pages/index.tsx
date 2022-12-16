50f319eccd107af29d6816ec4a34b0172fadaaa5
calmery <calmery@users.noreply.github.com>
2022-12-17 01:04:48 +0900

    画面上の領域を分割する

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,17 @@
+import classNames from "classnames";
+
 const Index = () => {
-  return <div className="text-center">Hello world</div>;
+  return (
+    <div
+      className={classNames("flex", "flex-col", "h-full", "justify-between")}
+    >
+      <menu className="shrink-0" type="toolbar">
+        Menu
+      </menu>
+      <section className="h-full">Hello world</section>
+      <nav className="shrink-0">Nav</nav>
+    </div>
+  );
 };
 
 export default Index;
