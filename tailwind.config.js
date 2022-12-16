b694b60da844eeb271f8bed605c25ad7d1021bb9
calmery <calmery@users.noreply.github.com>
2022-12-17 02:00:51 +0900

    Tailwind CSS で使用するフォントサイズと `backdrop-blur` の設定を上書きする

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 module.exports = {
   content: ["./src/**/*.{ts,tsx}"],
   theme: {
-    extend: {},
+    backdropBlur: {
+      DEFAULT: "0.25rem",
+    },
+    fontSize: {
+      12: ["0.75rem", "1rem"],
+      16: ["1rem", "1.25rem"],
+      20: ["1.25rem", "1.5rem"],
+    },
   },
   plugins: [],
 };
